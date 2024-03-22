69f9a12e5cfd6269593fe849c36f502f56796834
Faizal <faizalamri15@gmail.com>
2024-03-22 21:09:14 +0700

    update port

--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"final-project/config"
 	"final-project/database"
+	"os"
 )
 
 func main() {
-	Port := ":8080"
+	Port := envPortOr("8080")
 	database.StartDB()
 	config.StartServer().Run(Port)
 }
+
+func envPortOr(port string) string {
+	if envPort := os.Getenv("PORT"); envPort != "" {
+	  return ":" + envPort
+	}
+	return ":" + port
+  }
