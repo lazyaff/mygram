4a601b9c5fbf145b6a4405e219904fdf6d6be265
Faizal <faizalamri15@gmail.com>
2024-03-22 11:09:35 +0700

    set port

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
+import (
+	"final-project/config"
+	"final-project/database"
+)
+
 func main() {
-	// Port := ":8080"
-	// database.StartDB()
-	// config.StartServer().Run(Port)
+	Port := ":8080"
+	database.StartDB()
+	config.StartServer().Run(Port)
 }
